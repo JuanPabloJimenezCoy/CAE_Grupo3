f748756cf4f8d000b2c9aa231f97dd3c065bb0e3
JuanPabloJimenezCoy <juan.jimenez7771@unaula.edu.co>
2025-03-20 21:57:43 -0500

    feed: update back.py and Registro.html

--- ControlDeAccesoDeEmpleados/back.py
+++ ControlDeAccesoDeEmpleados/back.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+
 app = Flask(__name__, template_folder="Frontend")
 app.secret_key = 'supersecreto'
-empleados={}
+empleados = {}
 
 @app.route("/")
 def home():
@@ -9,26 +10,34 @@ def home():
 
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
-    num_tarjeta = request.args.get('num_tarjeta')
-    if num_tarjeta:
-        empleados[num_tarjeta] = {'num_tarjeta':num_tarjeta}
-        print("Empleados registrados:", empleados)
-        return f"empleado con tarjeta {num_tarjeta} registrado correctamente."
-    return "Error: Tiene que poner el numero de la tarjeta.", 400
+    if request.method == 'POST':
+        num_tarjeta = request.form.get('num_tarjeta')
+        nombre = request.form.get('nombre')
 
-@app.route('/login', methods = ['GET', 'POST'])
+        if num_tarjeta and nombre:
+            empleados[num_tarjeta] = {'num_tarjeta': num_tarjeta, 'nombre': nombre}
+            print("Empleado registrado:", empleados)
+            return redirect(url_for('login'))
+        return "Error: Debes completar todos los campos.", 400
+
+    return render_template('Registro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    num_tarjeta = request.form['num_tarjeta']
+    if request.method == 'POST':
+        num_tarjeta = request.form.get('num_tarjeta')
 
-    if num_tarjeta in empleados:
-        session['usuario'] = empleados[num_tarjeta]
-        return redirect(url_for('inicio'))
-    return "Error: Tarjeta no registrada"
+        if num_tarjeta in empleados:
+            session['usuario'] = empleados[num_tarjeta]
+            return redirect(url_for('inicio'))
+        return "Error: Tarjeta no registrada", 400
+
+    return render_template("Login.html")
 
 @app.route('/inicio')
 def inicio():
     if 'usuario' in session:
-        return render_template('Inicio.html', usuario=session['usuario'])
+        return render_template('Inicio.html', usuario=session['usuario']['nombre'])  
     return redirect(url_for('home'))
 
 @app.route('/salir')
@@ -36,5 +45,5 @@ def salir():
     session.pop('usuario', None)
     return redirect(url_for('home'))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
