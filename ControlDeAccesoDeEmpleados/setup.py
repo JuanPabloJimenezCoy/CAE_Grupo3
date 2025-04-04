1164f18a1381b3891708c6c6140431a43d692725
JuanPabloJimenezCoy <juan.jimenez7771@unaula.edu.co>
2025-04-04 16:04:17 -0500

    feat: create setup.py and update realse.please.yml

---
+++ ControlDeAccesoDeEmpleados/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="control-de-acceso",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+    ],
+    description="Sistema de control de acceso de empleados en Flask",
+    author="Grupo3",
+    author_email="Grupo3@gmail.com",
+    license="MIT",
+)
