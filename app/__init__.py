4034befa317ddb80f8608e34b162b1986502d535
JuanPabloJimenezCoy <juan.jimenez7771@unaula.edu.co>
2025-04-26 08:44:56 -0500

    feat: update __init__.py, login.html, revisar_tiempo_extra.html and create style_revisar_horas_extra.html

--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,13 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
-    app.config['AVISOS_FOLDER'] = os.path.join(app.config['UPLOAD_FOLDER'], 'avisos')
-    app.config['TIEMPO_EXTRA_FOLDER'] = os.path.join(app.config['UPLOAD_FOLDER'], 'tiempo_extra')
+
+    # ✅ Ahora sí correctamente apuntamos a /app/uploads
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
     app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'png', 'jpg', 'jpeg', 'doc', 'docx'}
-    os.makedirs(app.config['AVISOS_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['TIEMPO_EXTRA_FOLDER'], exist_ok=True)
+
+    # ✅ Creamos solo uploads si no existe (no más subcarpetas automáticas)
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     app.register_blueprint(main_bp)
     return app
